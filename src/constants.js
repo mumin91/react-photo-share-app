2222c4ae5e23e97f812db3468998e7972948d611
Muminur Rahman <r.muminur@gmail.com>
2020-03-02 21:59:29 +0600

    Change base URL

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://express-photo-catalog.herokuapp.com/';
-export const BASE_URL = 'http://localhost:4000/';
+export const BASE_URL = 'https://express-photo-catalog.herokuapp.com/';
+//export const BASE_URL = 'http://localhost:4000/';
 export const GET_POST_PHOTOS_URL = `${BASE_URL}photos`;
 export const DELETE_PHOTO_URL = `${BASE_URL}photo/`;
 export const UPLOADED_PHOTOS_URL = `${BASE_URL}images/uploads/`
